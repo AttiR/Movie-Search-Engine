944e0a08ab0e4bbc2e71c475e6cdeacb13785a36
AttiR <77799634+AttiR@users.noreply.github.com>
2021-09-27 23:04:46 +0300

    third commit

--- movieSearchBar.php
+++ movieSearchBar.php
@@ -5,7 +5,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/salika.css">
+
+  <link rel="stylesheet" href="css/salika.css?<?php echo time(); ?>">
+
   <!-- Boostsrap Css -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
   <!-- Boostsrap JavaScript Bundle -->
@@ -23,59 +25,75 @@
     <header class="header">
       <h1 id="title" class="text-center">Find your Favourite Movie</h1>
       <p id="description" class="description text-center">
-        Thank you for suing App, Search your Favourite Movie!
+        Thank you for using the App, Search your Favourite Movie!
       </p>
     </header>
+    <div class="container">
 
-    <form action="" method="post">
-      <input type="text" name="search" placeholder="enter movie name or some key" aria-label="Search"><br>
-      <button style="margin: 10px auto 10px auto;" class="btn btn-outline-success" type="submit" name="submit" value="search">Search</button>
-    </form>
+      <form action="" method="post">
+        <input type="text" name="search" placeholder="enter movie name or some key" aria-label="Search"><br>
+        <button style="margin: 10px auto 10px auto;" class="btn btn-outline-success" type="submit" name="submit" value="search">Search</button>
+      </form>
 
-    <table>
-      <tr>
-        <th>Sr.No.</th>
-        <th>Title</th>
-        <th>Description</th>
-        <th>Rating</th>
-        <th>release_year</th>
-
-      </tr>
-
-      <?php
-
-      // connecting to the database
-
-      include 'connectdb.php';
-      $search = $_POST["search"];
-      $query = "SELECT * FROM film where title like '$search%' OR 
-      description LIKE '$search%'";
-      /*$query = " SELECT * FROM `film` AS f INNER JOIN film_category AS fc ON fc.film_id = f.film_id INNER JOIN category AS c ON c. category_id = fc.category_id WHERE name LIKE '$search_value%' OR 
-      description LIKE '$search_value%' ";*/
-      $result = mysqli_query($conn, $query);
-      $no = 1;
-
-
-      while ($data = mysqli_fetch_array($result)) {
-
-      ?>
-
-
+      <table class="table">
         <tr>
-          <td><?php echo $no; ?></td>
-          <td><?php echo $data['title']; ?></td>
-          <td><?php echo $data['description']; ?></td>
-          <td><?php echo $data['rating']; ?></td>
-          <td><?php echo $data['release_year']; ?></td>
+          <th>Sr.No.</th>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Rating</th>
+          <th>release_year</th>
 
         </tr>
-      <?php
-        $no = $no + 1;
-      }
 
-      $conn->close();
-      ?>
-    </table>
+        <?php
+
+        // connecting to the database
+
+        session_start();
+        require_once("connectdb.php");
+
+        //include 'connectdb.php';
+        //$search = $_POST["search"];
+        $search = $conn->real_escape_string($_POST["search"]);
+        $query = "SELECT * FROM film where title like '$search%' OR 
+      description LIKE '$search%'";
+        /*$query = " SELECT * FROM `film` AS f INNER JOIN film_category AS fc ON fc.film_id = f.film_id INNER JOIN category AS c ON c. category_id = fc.category_id WHERE name LIKE '$search_value%' OR 
+      description LIKE '$search_value%' ";*/
+        $result = mysqli_query($conn, $query);
+        $no = 1;
+
+
+        while ($data = mysqli_fetch_array($result)) {
+
+        ?>
+
+
+          <tr>
+            <td><?php echo $no; ?></td>
+            <td><?php echo $data['title']; ?></td>
+            <td><?php echo $data['description']; ?></td>
+            <td><?php echo $data['rating']; ?></td>
+            <td><?php echo $data['release_year']; ?></td>
+
+          </tr>
+        <?php
+          $no = $no + 1;
+        }
+
+        if (isset($_SESSION['message'])) {
+          echo $_SESSION['message'];
+          unset($_SESSION['message']);
+        }
+
+        $conn->close();
+
+
+        ?>
+      </table>
+
+    </div>
+
+
 
   </center>
   <script>
